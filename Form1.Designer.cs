58c3c6ddac448ebf6996a1bd023390207de3f0c4
Nicolas Spalla <NicolasASpalla@gmail.com>
2025-09-28 20:26:21 +0200

    Agregar gitignore y subir proyecto limpio

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ToDoList_SQLv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTareas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dataGridTareas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTareas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblInicio.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblInicio.Location = new System.Drawing.Point(13, 13);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(775, 101);
+            this.lblInicio.TabIndex = 0;
+            this.lblInicio.Text = "ToDoList con Persistencia en SQL";
+            this.lblInicio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nueva tarea:";
+            // 
+            // txtTareas
+            // 
+            this.txtTareas.Location = new System.Drawing.Point(12, 142);
+            this.txtTareas.Multiline = true;
+            this.txtTareas.Name = "txtTareas";
+            this.txtTareas.Size = new System.Drawing.Size(704, 57);
+            this.txtTareas.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tareas registradas:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnGuardar.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F);
+            this.btnGuardar.Location = new System.Drawing.Point(12, 206);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(223, 31);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBorrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(722, 142);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(66, 57);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnModificar.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F);
+            this.btnModificar.Location = new System.Drawing.Point(241, 206);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(233, 31);
+            this.btnModificar.TabIndex = 6;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEliminar.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F);
+            this.btnEliminar.Location = new System.Drawing.Point(480, 206);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(236, 31);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dataGridTareas
+            // 
+            this.dataGridTareas.AllowUserToAddRows = false;
+            this.dataGridTareas.AllowUserToDeleteRows = false;
+            this.dataGridTareas.AllowUserToResizeColumns = false;
+            this.dataGridTareas.AllowUserToResizeRows = false;
+            this.dataGridTareas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridTareas.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTareas.Location = new System.Drawing.Point(13, 263);
+            this.dataGridTareas.Name = "dataGridTareas";
+            this.dataGridTareas.RowHeadersWidth = 51;
+            this.dataGridTareas.RowTemplate.Height = 24;
+            this.dataGridTareas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridTareas.Size = new System.Drawing.Size(775, 389);
+            this.dataGridTareas.TabIndex = 8;
+            this.dataGridTareas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridTareas_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 664);
+            this.Controls.Add(this.dataGridTareas);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTareas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInicio);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTareas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTareas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dataGridTareas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList_SQLv1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString =
+                "Data Source=.\\SQLEXPRESS;Initial Catalog=ToDoDB;Integrated Security=True;";
+       
+        private DataTable taskTable;
+        private bool isEditing = false;
+        private int idTemp = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            loadTask();
+            dataGridTareas.ClearSelection();
+            dataGridTareas.MultiSelect = false;
+            dataGridTareas.DataBindingComplete += DataGridTareas_DataBindingComplete;
+            dataGridTareas.Columns["Descripcion"].ReadOnly = true;
+        }
+        private void DataGridTareas_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dataGridTareas.ClearSelection(); // quita selección al terminar el binding
+        }
+
+        private void loadTask()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT Id, Descripcion, IsCompleted FROM Tareas", conn);
+                taskTable = new DataTable();
+                da.Fill(taskTable);
+                dataGridTareas.DataSource = taskTable;
+                //dataGridTareas.Columns["Id"].Visible = false;
+                dataGridTareas.AllowUserToAddRows = false;
+                
+            }
+            if (dataGridTareas.Columns["IsCompleted"] is DataGridViewCheckBoxColumn == false) 
+            {
+                dataGridTareas.Columns["IsCompleted"].ReadOnly = false;
+            }
+        }
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtTareas.Text = "";
+            isEditing = false;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTareas.Text) && isEditing == false)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Tareas (Descripcion) VALUES (@desc)", conn);
+                    cmd.Parameters.AddWithValue("@desc", txtTareas.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                txtTareas.Clear();
+                loadTask();
+            }
+            else if (isEditing == true)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Tareas SET Descripcion=@desc WHERE Id=@id",conn);
+                    cmd.Parameters.AddWithValue("@desc",txtTareas.Text);
+                    cmd.Parameters.AddWithValue("@id", idTemp);
+                    cmd.ExecuteNonQuery();
+                }
+                txtTareas.Clear();
+                loadTask();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridTareas.CurrentRow != null) 
+            {
+                DialogResult result = MessageBox.Show(
+                    "Esta seguro que desea elimiar?",
+                    "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes) 
+                {
+                    int id = (int)dataGridTareas.CurrentRow.Cells["Id"].Value;
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand("DELETE FROM Tareas where Id=@desc", conn);
+                        cmd.Parameters.AddWithValue("@desc", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                    loadTask();
+                }
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dataGridTareas.CurrentRow != null)
+            {
+                string tareaSeleccionada = (string)dataGridTareas.CurrentRow.Cells["Descripcion"].Value;
+                txtTareas.Text = tareaSeleccionada;
+                isEditing = true;
+                idTemp = (int)dataGridTareas.CurrentRow.Cells["Id"].Value;
+            }
+        }
+
+        private void dataGridTareas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0 && dataGridTareas.Columns[e.ColumnIndex].Name == "IsCompleted") 
+            {
+                DataGridViewRow row = dataGridTareas.Rows[e.RowIndex];
+                bool currentValue = (bool)row.Cells["IsCompleted"].Value;
+
+                DialogResult result = MessageBox.Show(!currentValue ? "Esta seguro que ha completado la tarea?" :
+                    "Esta seguro que quiere desmarcar la tarea?", "Confirmar cambio",
+                     MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes) 
+                {
+                    int id = (int)row.Cells["Id"].Value;
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand("UPDATE Tareas SET IsCompleted=@completada WHERE Id=@id", conn);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@completada", !currentValue);
+                        cmd.ExecuteNonQuery();
+                    }
+                    row.Cells["IsCompleted"].Value = currentValue;
+                }
+                else if (result == DialogResult.No) 
+                {
+                    loadTask();                   
+                }
+            }
+        }
+    }
+}
+
